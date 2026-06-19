e56b9488fcb1114014aa523c620ab0aea024aa5b
Alik977 <alik.hakobyan.sh@gmail.com>
2026-06-20 03:00:28 +0400

    exp

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,36 @@
-
+import "./Experience.css";
 function Experience() {
-    return(
-        <div>
-
-            
+  return (
+    <div>
+      <div className="experienc">
+        <div className="allexpreienc">
+          <h1>EXPERIENCE</h1>
+          <div className="expparagraph">
+            <div className="part1">
+              <h3>Frontend Intern</h3>
+              <p>SmartCode / 3 Months</p>
+            </div>
+            <div className="part2">
+              <p>
+                Developed a responsive bicycle shop website page with a clean
+                layout and modern UI design. Built a To-Do List application
+                using JavaScript and React, implementing dynamic task management
+                and state updates. Created an interactive Tic-Tac-Toe (X-O) game
+                with functional game logic and user interaction. Designed a
+                football club website page inspired by Real Madrid, focusing on
+                structured content and modern layout. Built a prototype website
+                inspired by BMW, emphasizing clean UI design and visual
+                presentation. Developed a design studio website that showcases
+                creative services such as naming, branding, and design
+                solutions, featuring a modern layout, responsive design, and
+                clear navigation to improve user experience.
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
